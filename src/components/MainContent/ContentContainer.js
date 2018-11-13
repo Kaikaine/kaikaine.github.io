7b9b1f97fc1cdaf393cf99f680a163db50e60fac
Kairi <kairijb@gmail.com>
2018-11-12 21:15:42 -0600

    moved profile pic to content container so it renders on each page

--- src/components/MainContent/ContentContainer.js
+++ src/components/MainContent/ContentContainer.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import { Route} from 'react-router-dom';
+import './content.css'
+import avi from '../../avi.jpg'
+
+
+const spin = () => {
+    if (document.querySelector('.avi').classList.contains('angry-animate')) {
+        document.querySelector('.avi').classList.remove('angry-animate')
+    }
+    else {
+        document.querySelector('.avi').classList.add('angry-animate')
+    }
+}
 
 const ContentContainer = () => {
     return ( <div>
-        <About />
-        
+        <img onClick={spin} className='avi' src={avi} alt='Profile pic'/>
+        <Route exact path="/" component={About} />
+        <Route expact path='/portfolio' component={Projects} />
+        <Route exact path='/contact' component={Contact} />
         </div> );
 }
  
